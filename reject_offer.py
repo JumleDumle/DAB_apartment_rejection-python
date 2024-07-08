75f12214832d049ab2218fb0dc03df429ac99458
Julius V. <juliusv135@gmail.com>
2024-07-08 22:01:01 +0200

    Fixed last dialog making confirm dialog unable to be located and clicked

--- reject_offer.py
+++ reject_offer.py
@@ -34,32 +34,32 @@ def show_offer():
     pos = imagesearcharea(se_tilbud, 1388, 606, 1529, 684)
     if pos[0] != -1:
         print("position : ", pos[0], pos[1])
-        time.sleep(1)
+        time.sleep(0.5)
         click(1444, 653)
-        time.sleep(2)
+        time.sleep(1.5)
     else:
         raise FileNotFoundError(F"image {se_tilbud} not found on screen")
 
 # Define function to decline offer
 def decline_offer():
-    pos = imagesearcharea(afvis_tilbud, 1412, 987, 1511, 1044)
+    pos = imagesearcharea(afvis_tilbud, 1405, 950, 1511, 1044)
     if pos[0] != -1:
         print("position : ", pos[0], pos[1])
-        click(1461, 1010)
-        time.sleep(1)
+        click(1461, 1008)
+        time.sleep(0.9)
         pyautogui.press('pagedown')
-        time.sleep(3)
+        time.sleep(1.5)
     else:
         raise FileNotFoundError(F"image {afvis_tilbud} not found on screen")
 
 # Define function to confirm
 def confirm_no():
-    pos = imagesearcharea(bekræft_nej, 1338, 395, 1433, 458)
+    pos = imagesearcharea(bekræft_nej, 1338, 375, 1433, 488)
     if pos[0] != -1:
         print("position : ", pos[0], pos[1])
-        time.sleep(1)
-        click(1390, 422)
-        time.sleep(8)
+        time.sleep(0.3)
+        click(1390, 420)
+        time.sleep(5.3)
         pyautogui.press('home')
     else:
         raise FileNotFoundError(F"image {bekræft_nej} not found on screen")
@@ -72,16 +72,14 @@ def main():
 #    mouse_pos()
 
     # amount of offers to decline (can be found under: https://www.dabbolig.dk/min-side/min-boligsoegning/)
-    offer_count = 24
+    offer_count = 1
     # THE loop
     while offer_count > 0:
-        show_offer()
+        """show_offer()
         decline_offer()
-        confirm_no()
-        offer_count - 1
-        if offer_count == 0:
-            print("DONE!")
-            break
+        confirm_no()"""
+        offer_count =- 1
+    print("DONE!!!")
 
 
 if __name__ == "__main__":
